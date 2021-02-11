00191626d0430b1a81f4cb627f1cc09d4317101d
Nursultanosmonov13 <nursultanosmonov93@gmail.com>
2021-02-11 23:19:48 +0600

    SS

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,19 +5,20 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        HavingSuperAbility[] havingSuperAbilities = {new Magic(), new Warrior(),new Medic()};
-       // System.out.println(havingSuperAbilities);
-        for (int i = 0; i < havingSuperAbilities.length ; i++) {
+        HavingSuperAbility[] havingSuperAbilities = {new Magic(), new Warrior(), new Medic()};
+        // System.out.println(havingSuperAbilities);
+        for (int i = 0; i < havingSuperAbilities.length; i++) {
             herAll(havingSuperAbilities[i]);
 
 
         }
 
     }
-    public static void herAll(HavingSuperAbility havingSuperAbility){
+
+    public static void herAll(HavingSuperAbility havingSuperAbility) {
 
         System.out.println(havingSuperAbility.applySuperAbility(" CRITICAL DAMAGE"));
-       // System.out.println(havingSuperAbility.applySuperAbility("dd"));
+        // System.out.println(havingSuperAbility.applySuperAbility("dd"));
     }
 
 }
